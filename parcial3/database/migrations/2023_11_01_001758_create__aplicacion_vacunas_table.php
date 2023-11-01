d7ffbd35f5f5ee217bea0eadfb35e179db7a6f59
GOrtiz04 <gabyortiz2013@gmail.com>
2023-10-31 18:59:40 -0600

    parcial

---
+++ parcial3/database/migrations/2023_11_01_001758_create__aplicacion_vacunas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('_aplicacion_vacunas', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('persona_id');
+            $table->unsignedBigInteger('marca_id');
+            $table->unsignedBigInteger('dosis_id');
+            $table->timestamps();
+
+            $table->foreign('persona_id')->references('id')->on('personas');
+            $table->foreign('marca_id')->references('id')->on('marcas');
+            $table->foreign('dosis_id')->references('id')->on('dosis');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('_aplicacion_vacunas');
+    }
+};
